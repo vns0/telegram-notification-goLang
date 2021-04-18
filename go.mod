e3b568caf7c6b9094d2d5419a53ac34b13da752b
Nikita Vtorushin <n.vtorushin@inbox.ru>
2021-04-18 15:54:28 +0700

    create api /notify/send with data body

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.16
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
 	"encoding/json"
-	"github.com/joho/godotenv"
+	"fmt"
 	"io/ioutil"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 type env struct {
@@ -17,7 +18,17 @@ type env struct {
 }
 
 type jsonDB struct {
-	Ids         []string
+	Ids []string
+}
+
+type messageDB struct {
+	Ids     []string `json:"ids"`
+	Message string   `json:"message"`
+}
+
+type responseMessage struct {
+	Success bool
+	Message string
 }
 
 var (
@@ -43,25 +54,48 @@ func loadConfig() *jsonDB {
 		log.Fatalf("Error: %s", err)
 	}
 	fmt.Println("Successfully Opened data.json")
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
 	var result jsonDB
 	content, err := ioutil.ReadFile("data.json")
-	checkError(err)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
 	json.Unmarshal([]byte(content), &result)
 	return &result
 }
 
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
+func handleSend(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var message messageDB
+	_ = json.NewDecoder(r.Body).Decode(&message)
+	response := getPaylod(message)
+	if !response.Success {
+		w.WriteHeader(http.StatusBadRequest)
 	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func getPaylod(message messageDB) responseMessage {
+	var response responseMessage
+	response.Success = true
+	response.Message = "Message sended"
+	if message.Ids == nil {
+		message.Ids = Data.Ids
+	}
+	if message.Message == "" {
+		response.Message = "No required field - message"
+		response.Success = false
+	}
+	return response
 }
 
 func main() {
+	r := mux.NewRouter()
 	dotenv := env{idBot: getEnv("telegram_id"), port: getEnv("port")}
 	fmt.Println(dotenv.idBot)
 	Data = loadConfig()
 	fmt.Println(Data.Ids)
+	r.HandleFunc("/notify/send", handleSend).Methods("POST")
+	log.Fatal(http.ListenAndServe(":"+dotenv.port, r))
 }
